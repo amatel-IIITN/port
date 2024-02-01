7e0a94b24e4880719ae02fd37c3b7dcc36a4462e
Aman Patel <128774540+amatel-IIITN@users.noreply.github.com>
2024-02-01 12:44:28 +0530

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,78 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+
+
+
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+
+window.onscroll = () => {
+
+sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height) {
+        navlinks.forEach(links => {
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        });
+    };
+});
+
+let header = document.querySelector('.header');
+
+header.classList.toggle('sticky', window.scrollY > 100);
+
+menuIcon.classList.remove('bx-x');
+menuIcon.classList.remove('active');
+
+
+};
+
+
+// darkmode lightmode javascript
+
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = () => {
+    darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
+};
+
+// function toggleReadMore() {
+//     var partialParagraph = document.getElementById('partialParagraph');
+//     var fullParagraph = document.getElementById('fullParagraph');
+  
+//     // Toggle the visibility of the paragraphs
+//     partialParagraph.classList.toggle('hiddens');
+//     fullParagraph.classList.toggle('hiddens');
+// }
+
+
+
+// scroll reveal section javascript
+
+
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
